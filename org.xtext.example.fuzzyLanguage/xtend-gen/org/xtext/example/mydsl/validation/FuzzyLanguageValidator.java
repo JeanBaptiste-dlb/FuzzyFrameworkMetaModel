4f9963c3053f5759456dd0b59fb042dba437429c
Cormak <jeanbaptistedelabroise@gmail.com>
2020-05-04 23:09:54 +0200

    validator

--- org.xtext.example.fuzzyLanguage/xtend-gen/org/xtext/example/mydsl/validation/FuzzyLanguageValidator.java
+++ org.xtext.example.fuzzyLanguage/xtend-gen/org/xtext/example/mydsl/validation/FuzzyLanguageValidator.java
@@ -3,6 +3,16 @@
  */
 package org.xtext.example.mydsl.validation;
 
+import com.google.common.collect.Iterators;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.xtend2.lib.StringConcatenation;
+import org.eclipse.xtext.validation.Check;
+import org.eclipse.xtext.xbase.lib.IteratorExtensions;
+import org.xtext.example.mydsl.fuzzyLanguage.Affectation;
+import org.xtext.example.mydsl.fuzzyLanguage.Expression;
+import org.xtext.example.mydsl.fuzzyLanguage.FuzzyLanguagePackage;
+import org.xtext.example.mydsl.fuzzyLanguage.Output;
+import org.xtext.example.mydsl.fuzzyLanguage.impl.TypeSetImpl;
 import org.xtext.example.mydsl.validation.AbstractFuzzyLanguageValidator;
 
 /**
@@ -12,4 +22,152 @@ import org.xtext.example.mydsl.validation.AbstractFuzzyLanguageValidator;
  */
 @SuppressWarnings("all")
 public class FuzzyLanguageValidator extends AbstractFuzzyLanguageValidator {
+  private String type;
+  
+  @Check
+  public String checkTypeImpl(final TypeSetImpl impl) {
+    return this.type = impl.getTypeName();
+  }
+  
+  @Check
+  public void checkAffectation(final Affectation a) {
+    String _value = a.getValue();
+    boolean _tripleNotEquals = (_value != null);
+    if (_tripleNotEquals) {
+      boolean _equals = a.getValue().equals("Gaussian");
+      if (_equals) {
+        int _size = a.getParams().size();
+        boolean _tripleNotEquals_1 = (_size != 2);
+        if (_tripleNotEquals_1) {
+          this.error("Gaussian function requires 2 parameters : center:T and stddev:T", 
+            FuzzyLanguagePackage.Literals.AFFECTATION__VALUE);
+          return;
+        }
+      }
+      boolean _equals_1 = a.getValue().equals("Ramp");
+      if (_equals_1) {
+        int _size_1 = a.getParams().size();
+        boolean _tripleNotEquals_2 = (_size_1 != 3);
+        if (_tripleNotEquals_2) {
+          StringConcatenation _builder = new StringConcatenation();
+          _builder.append("Ramp function requires 3 parameters : min:");
+          _builder.append(this.type);
+          _builder.append(", max:");
+          _builder.append(this.type);
+          _builder.append(" and direction");
+          this.error(_builder.toString(), 
+            FuzzyLanguagePackage.Literals.AFFECTATION__VALUE);
+          return;
+        }
+        if (((!a.getParams().get(2).getValue().equals("Up")) && (!a.getParams().get(2).getValue().equals("Down")))) {
+          this.error("The third parameter has to be a direction : Up or Down", FuzzyLanguagePackage.Literals.AFFECTATION__VALUE);
+          return;
+        }
+      }
+      boolean _equals_2 = a.getValue().equals("Triangle");
+      if (_equals_2) {
+        int _size_2 = a.getParams().size();
+        boolean _tripleNotEquals_3 = (_size_2 != 3);
+        if (_tripleNotEquals_3) {
+          StringConcatenation _builder_1 = new StringConcatenation();
+          _builder_1.append("Triangle function requires 3 parameters : min:");
+          _builder_1.append(this.type);
+          _builder_1.append(" , mid:");
+          _builder_1.append(this.type);
+          _builder_1.append("  and max:");
+          _builder_1.append(this.type);
+          _builder_1.append(" ");
+          this.error(_builder_1.toString(), 
+            FuzzyLanguagePackage.Literals.AFFECTATION__VALUE);
+          return;
+        }
+      }
+      boolean _equals_3 = a.getValue().equals("Trapeze");
+      if (_equals_3) {
+        int _size_3 = a.getParams().size();
+        boolean _tripleNotEquals_4 = (_size_3 != 5);
+        if (_tripleNotEquals_4) {
+          StringConcatenation _builder_2 = new StringConcatenation();
+          _builder_2.append("Trapeze function requires 5 parameters : min:");
+          _builder_2.append(this.type);
+          _builder_2.append(" , left:");
+          _builder_2.append(this.type);
+          _builder_2.append(" , right:");
+          _builder_2.append(this.type);
+          _builder_2.append(" , max:");
+          _builder_2.append(this.type);
+          _builder_2.append("  and concavity");
+          this.error(_builder_2.toString(), 
+            FuzzyLanguagePackage.Literals.AFFECTATION__VALUE);
+          return;
+        }
+        if (((!a.getParams().get(4).equals("CONCAVE")) && a.getParams().get(4).equals("CONVEXE"))) {
+          this.error("Must be a concavity : CONCAVE or CONVEXE", FuzzyLanguagePackage.Literals.AFFECTATION__PARAMS);
+          return;
+        }
+      }
+    }
+    for (int i = 0; (i < IteratorExtensions.<Affectation>toList(Iterators.<Affectation>filter(a.eContainer().eAllContents(), Affectation.class)).size()); i++) {
+      boolean _equals_4 = IteratorExtensions.<Affectation>toList(Iterators.<Affectation>filter(a.eContainer().eAllContents(), Affectation.class)).get(i).getName().equals(a.getName());
+      if (_equals_4) {
+        Affectation _get = IteratorExtensions.<Affectation>toList(Iterators.<Affectation>filter(a.eContainer().eAllContents(), Affectation.class)).get(i);
+        boolean _tripleNotEquals_5 = (_get != a);
+        if (_tripleNotEquals_5) {
+          String _name = a.getName();
+          String _plus = ("Be carefull, \"" + _name);
+          String _plus_1 = (_plus + "\" has already been defined");
+          this.warning(_plus_1, FuzzyLanguagePackage.Literals.AFFECTATION__NAME);
+        }
+      }
+    }
+  }
+  
+  @Check
+  public void checkExpression(final Expression e) {
+    if ((((((e.getName().equals("AND") || e.getName().equals("NOT")) || e.getName().equals("OR")) || e.getName().equals("DEFUZZ")) || 
+      e.getName().equals("THEN")) || e.getName().equals("AGG"))) {
+      int _size = e.getExps().size();
+      boolean _tripleNotEquals = (_size != 2);
+      if (_tripleNotEquals) {
+        this.error("This operator requires 2 expressions", FuzzyLanguagePackage.Literals.EXPRESSION__NAME);
+        return;
+      }
+    } else {
+      boolean _IsNameDefine = this.IsNameDefine(e);
+      boolean _not = (!_IsNameDefine);
+      if (_not) {
+        this.error("You can\'t use this name because it has not been define yet", FuzzyLanguagePackage.Literals.EXPRESSION__NAME);
+        return;
+      }
+    }
+  }
+  
+  public boolean IsNameDefine(final Expression e) {
+    EObject parent = e.eContainer();
+    while ((!parent.getClass().toString().equals(
+      "class org.xtext.example.mydsl.fuzzyLanguage.impl.FuzzySystemBuildingImpl"))) {
+      parent = parent.eContainer();
+    }
+    for (int i = 0; (i < IteratorExtensions.<Affectation>toList(Iterators.<Affectation>filter(parent.eAllContents(), Affectation.class)).size()); i++) {
+      boolean _equals = IteratorExtensions.<Affectation>toList(Iterators.<Affectation>filter(parent.eAllContents(), Affectation.class)).get(i).getName().equals(e.getName());
+      if (_equals) {
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  @Check
+  public void checkOutput(final Output o) {
+    String _path = o.getPath();
+    boolean _tripleNotEquals = (_path != null);
+    if (_tripleNotEquals) {
+      String str1 = "\"";
+      String str2 = "\'";
+      if (((o.getPath().toString().charAt(0) != str1.charAt(0)) && (o.getPath().toString().charAt(0) != str2.charAt(0)))) {
+        this.error("You have to indicate a path like : \"folder/file.extension\"", FuzzyLanguagePackage.Literals.OUTPUT__PATH);
+        return;
+      }
+    }
+  }
 }
