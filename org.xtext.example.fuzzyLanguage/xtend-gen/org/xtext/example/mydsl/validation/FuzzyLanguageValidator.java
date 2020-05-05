8a9f1425c887177f313196bddb0d7924aa87af7f
Cormak <jeanbaptistedelabroise@gmail.com>
2020-05-05 13:55:15 +0200

    validator modifié

--- org.xtext.example.fuzzyLanguage/xtend-gen/org/xtext/example/mydsl/validation/FuzzyLanguageValidator.java
+++ org.xtext.example.fuzzyLanguage/xtend-gen/org/xtext/example/mydsl/validation/FuzzyLanguageValidator.java
@@ -39,7 +39,12 @@ public class FuzzyLanguageValidator extends AbstractFuzzyLanguageValidator {
         int _size = a.getParams().size();
         boolean _tripleNotEquals_1 = (_size != 2);
         if (_tripleNotEquals_1) {
-          this.error("Gaussian function requires 2 parameters : center:T and stddev:T", 
+          StringConcatenation _builder = new StringConcatenation();
+          _builder.append("Gaussian function requires 2 parameters : center:");
+          _builder.append(this.type);
+          _builder.append(" and stddev:");
+          _builder.append(this.type);
+          this.error(_builder.toString(), 
             FuzzyLanguagePackage.Literals.AFFECTATION__VALUE);
           return;
         }
@@ -49,18 +54,18 @@ public class FuzzyLanguageValidator extends AbstractFuzzyLanguageValidator {
         int _size_1 = a.getParams().size();
         boolean _tripleNotEquals_2 = (_size_1 != 3);
         if (_tripleNotEquals_2) {
-          StringConcatenation _builder = new StringConcatenation();
-          _builder.append("Ramp function requires 3 parameters : min:");
-          _builder.append(this.type);
-          _builder.append(", max:");
-          _builder.append(this.type);
-          _builder.append(" and direction");
-          this.error(_builder.toString(), 
+          StringConcatenation _builder_1 = new StringConcatenation();
+          _builder_1.append("Ramp function requires 3 parameters : min:");
+          _builder_1.append(this.type);
+          _builder_1.append(", max:");
+          _builder_1.append(this.type);
+          _builder_1.append(" and direction");
+          this.error(_builder_1.toString(), 
             FuzzyLanguagePackage.Literals.AFFECTATION__VALUE);
           return;
         }
         if (((!a.getParams().get(2).getValue().equals("Up")) && (!a.getParams().get(2).getValue().equals("Down")))) {
-          this.error("The third parameter has to be a direction : Up or Down", FuzzyLanguagePackage.Literals.AFFECTATION__VALUE);
+          this.error("The 3rd parameter has to be a direction : Up or Down", FuzzyLanguagePackage.Literals.AFFECTATION__VALUE);
           return;
         }
       }
@@ -69,15 +74,15 @@ public class FuzzyLanguageValidator extends AbstractFuzzyLanguageValidator {
         int _size_2 = a.getParams().size();
         boolean _tripleNotEquals_3 = (_size_2 != 3);
         if (_tripleNotEquals_3) {
-          StringConcatenation _builder_1 = new StringConcatenation();
-          _builder_1.append("Triangle function requires 3 parameters : min:");
-          _builder_1.append(this.type);
-          _builder_1.append(" , mid:");
-          _builder_1.append(this.type);
-          _builder_1.append("  and max:");
-          _builder_1.append(this.type);
-          _builder_1.append(" ");
-          this.error(_builder_1.toString(), 
+          StringConcatenation _builder_2 = new StringConcatenation();
+          _builder_2.append("Triangle function requires 3 parameters : min:");
+          _builder_2.append(this.type);
+          _builder_2.append(" , mid:");
+          _builder_2.append(this.type);
+          _builder_2.append("  and max:");
+          _builder_2.append(this.type);
+          _builder_2.append(" ");
+          this.error(_builder_2.toString(), 
             FuzzyLanguagePackage.Literals.AFFECTATION__VALUE);
           return;
         }
@@ -87,22 +92,22 @@ public class FuzzyLanguageValidator extends AbstractFuzzyLanguageValidator {
         int _size_3 = a.getParams().size();
         boolean _tripleNotEquals_4 = (_size_3 != 5);
         if (_tripleNotEquals_4) {
-          StringConcatenation _builder_2 = new StringConcatenation();
-          _builder_2.append("Trapeze function requires 5 parameters : min:");
-          _builder_2.append(this.type);
-          _builder_2.append(" , left:");
-          _builder_2.append(this.type);
-          _builder_2.append(" , right:");
-          _builder_2.append(this.type);
-          _builder_2.append(" , max:");
-          _builder_2.append(this.type);
-          _builder_2.append("  and concavity");
-          this.error(_builder_2.toString(), 
+          StringConcatenation _builder_3 = new StringConcatenation();
+          _builder_3.append("Trapeze function requires 5 parameters : min:");
+          _builder_3.append(this.type);
+          _builder_3.append(" , left:");
+          _builder_3.append(this.type);
+          _builder_3.append(" , right:");
+          _builder_3.append(this.type);
+          _builder_3.append(" , max:");
+          _builder_3.append(this.type);
+          _builder_3.append("  and concavity");
+          this.error(_builder_3.toString(), 
             FuzzyLanguagePackage.Literals.AFFECTATION__VALUE);
           return;
         }
-        if (((!a.getParams().get(4).equals("CONCAVE")) && a.getParams().get(4).equals("CONVEXE"))) {
-          this.error("Must be a concavity : CONCAVE or CONVEXE", FuzzyLanguagePackage.Literals.AFFECTATION__PARAMS);
+        if (((!a.getParams().get(4).getValue().equals("CONCAVE")) && (!a.getParams().get(4).getValue().equals("CONVEXE")))) {
+          this.error("The 5th parameter has to be a concavity : CONCAVE or CONVEXE", FuzzyLanguagePackage.Literals.AFFECTATION__VALUE);
           return;
         }
       }
@@ -167,6 +172,20 @@ public class FuzzyLanguageValidator extends AbstractFuzzyLanguageValidator {
       if (((o.getPath().toString().charAt(0) != str1.charAt(0)) && (o.getPath().toString().charAt(0) != str2.charAt(0)))) {
         this.error("You have to indicate a path like : \"folder/file.extension\"", FuzzyLanguagePackage.Literals.OUTPUT__PATH);
         return;
+      }
+      boolean _contains = o.getPath().toString().contains("/");
+      boolean _not = (!_contains);
+      if (_not) {
+        this.warning(
+          "Your path doesn\'t contain any \"/\" character, ensure you are indicating a real path like \"folder/file.extension\"", 
+          FuzzyLanguagePackage.Literals.OUTPUT__PATH);
+      }
+      boolean _contains_1 = o.getPath().toString().contains(".");
+      boolean _not_1 = (!_contains_1);
+      if (_not_1) {
+        this.warning(
+          "Your path doesn\'t contain a \".\" character, ensure you are indicating a real path like \"folder/file.extension\"", 
+          FuzzyLanguagePackage.Literals.OUTPUT__PATH);
       }
     }
   }
